9dc729725679102da6186fee97724d2864e969ae
imane <imane.meziany47@gmail.com>
2023-03-25 01:37:02 -0700

    r

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,10 @@ int i;
 long int b;
 long long int d;
 float f;
-printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\nSize of a long int: %lu byte(s)\nSize of a long long int: %lu size(s)\nSize of a float: %lu byte(s)",(unsigned long)sizeof(c),(unsigned long)sizeof(i),(unsigned long)sizeof(b),(unsigned long)sizeof(d),(unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
 return (0);
 }
