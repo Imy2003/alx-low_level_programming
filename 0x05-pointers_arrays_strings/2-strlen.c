3c4141a97415f4a48c9cc1b2e2719b92af25c8a2
imane <imane.meziany47@gmail.com>
2023-03-27 17:13:21 -0700

    r

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
- *
- * Return: The length of @str.
- */
-size_t _strlen(const char *str)
+  * _strlen - return the lenth of the string as an int value
+  * @s: the string to measure.
+  *
+  * Return: Int value of the lenth.
+  */
+int _strlen(char *s)
 {
-	size_t length = 0;
+	int count = 0;
 
-	while (*str != '\0')
-		length++;
-		str++;
-	return (length);
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	return (count);
 }
+
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,8 +6,8 @@
 
 int _putchar(char c);
 void reset_to_98(int *n);
+int _strlen(char *s);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -15,6 +15,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
 
 #endif
